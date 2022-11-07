f0217b9f5668db83167a39be073ad0a365e412b5
nico0704 <nico.schroeder@informatik.hs-fulda.de>
2022-11-07 23:49:43 +0100

    works

--- dct.js
+++ dct.js
@@ -1,5 +1,4 @@
-const WIDTH = 8;
-const HEIGHT = 8;
+const SIZE = 8;
 const RECIPROCAL_SQRT_2 = 1.0 / Math.sqrt(2);
 const PI = Math.PI;
 
@@ -13,17 +12,18 @@ function run_dct() {
     }
     const input_arr_2_dim = to_2_dim_arr(input_arr_1_dim);
     console.log(input_arr_2_dim);
+    document.getElementById("input_matrix").innerHTML = display_matrix(input_arr_2_dim);
 
     // dct
     var result_matrix = Array(8).fill(0).map(x => Array(8).fill(0));
     var u, v, cu, cv, j, k, sum;
-    for (u = 0; u < WIDTH; u++) {
-        for (v = 0; v < HEIGHT; v++) {
+    for (u = 0; u < SIZE; u++) {
+        for (v = 0; v < SIZE; v++) {
             cu = u == 0 ? RECIPROCAL_SQRT_2 : 1.0;
             cv = v == 0 ? RECIPROCAL_SQRT_2 : 1.0;
             sum = 0.0;
-            for (j = 0; j < 8; j++) {
-                for (k = 0; k < 8; k++) {
+            for (j = 0; j < SIZE; j++) {
+                for (k = 0; k < SIZE; k++) {
                     var tmp = input_arr_2_dim[j][k];
                     tmp *= Math.cos(((2 * j + 1) * u * PI) / 16);
                     tmp *= Math.cos(((2 * k + 1) * v * PI) / 16);
@@ -34,17 +34,32 @@ function run_dct() {
         }
     }
     console.log(result_matrix);
-    document.getElementById("output_matrix").innerHTML = result_matrix;
+    document.getElementById("output_matrix").innerHTML = display_matrix(result_matrix);
 }
 
 function to_2_dim_arr(input_arr_1_dim) {
     var resultArray = [];
-    for (var i = 0; i < input_arr_1_dim.length; i += WIDTH) {
+    for (var i = 0; i < input_arr_1_dim.length; i += SIZE) {
         let tmp = [];
-        for (var j = i; j < i + WIDTH; j++) {
+        for (var j = i; j < i + SIZE; j++) {
             tmp.push(input_arr_1_dim[j]);
         }
         resultArray.push(tmp);
     }
     return resultArray;
+}
+
+function display_matrix(matrix) {
+    var res = "";
+    for (var x = 0; x < SIZE; x++) {
+        for (var y = 0; y < SIZE; y++) {
+            if (y == SIZE - 1) {
+                res += matrix[x][y];
+            } else {
+                res += matrix[x][y] + "\t";
+            }
+        }
+        res += "\n";
+    }
+    return res;
 }
